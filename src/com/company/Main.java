a5c6534eb65d622e2f43e045016fc2d4d0bb7139
Josh <blkjosh923@gmail.com>
2021-04-11 21:12:51 -0500

    First commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,24 @@
 package com.company;
 
+
+import data_types.DataTypes;
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("hello world");
+
+        //Scanner and type casting
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("What's your name?");
+        String scannedName = sc.next();
+        System.out.println("Hello "+scannedName+"!");
+
+        System.out.println("May I ask your age?");
+        int scannedAge = sc.nextInt();
+        System.out.println(scannedName+", you are "+scannedAge+" years old!");
+
     }
 }
---
+++ src/data_types/DataTypes.java
@@ -0,0 +1,53 @@
+package data_types;
+
+public class DataTypes {
+
+    //primitive
+    private int num = 9;
+    private double number = 9.0;
+    private boolean b = true;
+    private char single = '9';
+    //non primitive
+    private String word = "Hello";
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public double getNumber() {
+        return number;
+    }
+
+    public void setNumber(double number) {
+        this.number = number;
+    }
+
+        public boolean getB() {
+        return b;
+        }
+
+    public void setB(boolean b) {
+        this.b = b;
+    }
+
+    public char getSingle() {
+        return single;
+    }
+
+    public void setSingle(char single){
+        this.single=single;
+
+    }
+
+    public String getWord(){
+        return word;
+    }
+
+    public void setWord(String word){
+        this.word =word;
+    }
+}
